a5175560c27bbab83f1f8525a5a39bde182150a6
Agustin.CHINCHILLA <agustin.chinchilla@naranja.com>
2022-01-30 18:36:41 -0300

    Fix scroll

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,22 +4,10 @@ import Work from "../Components/Work";
 import Projects from "../Components/Projects";
 import MoreProjects from "../Components/MoreProjects";
 import Contact from "../Components/Contact";
-import { useState } from "react";
 
 const Home = () => {
-  const [theme, setTheme] = useState("dark");
-
-  const btnTheme = () => {
-    if (theme === "dark") {
-      setTheme("ligth");
-    } else {
-      setTheme("dark");
-    }
-  };
-
   return (
     <>
-      {/*   <button onClick={btnTheme} >change theme</button> */}
       <main className="homeContainer">
         <Hero />
         <About />
