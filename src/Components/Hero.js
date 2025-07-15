57cd2b5473d0ee327bacb9b73d5e14ce9f324042
Agustin garcia <agustinchinchilla16@gmail.com>
2025-07-15 13:21:12 +0200

    Remove animation

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,34 +1,16 @@
 import { useRef, useEffect } from "react";
-import { gsap } from "gsap";
 import { useTranslation } from "react-i18next";
 const Hero = () => {
   const title = useRef();
-  const containerHero = useRef();
   const { t } = useTranslation();
-  useEffect(() => {
-    gsap.from(title.current, {
-      duration: 1.5,
-      rotate: 360,
-      repeat: 1,
-      yoyoEase: true,
-      ease: "elastic",
-    });
-    gsap.from(containerHero.current, {
-      duration: 1.5,
-      rotate: 360,
-      repeat: 1,
-      yoyoEase: true,
-      ease: "elastic",
-    });
-  }, []);
 
   return (
     <section id="inicio" className="containerHero">
       <h1 className="titleHero" ref={title}>
-        Agustin Garcia.
+        Agustin Chinchilla Garcia
       </h1>
-      <div ref={containerHero}>
-        <h2 className="subTitle"> iOS and web developer </h2>
+      <div>
+        <h2 className="subTitle"> iOS Engineer </h2>
         <p className="textHero">{t("text.hero")}</p>
         <a href="#contact" className="btnHero">
           {t("text.hero.btn")}
