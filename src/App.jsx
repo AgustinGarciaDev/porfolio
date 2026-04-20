c42e90298fa9e0b509b9f0ce682b3a80fe030612
Agustin garcia <agustinchinchilla16@gmail.com>
2026-04-20 19:27:13 -0300

    update legacy project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import "./scss/style.scss";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+};
+
+export default App;
